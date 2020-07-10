1578af7423f402adc5fc70653005097cce04584e
Eduardo Valencia <8025918@gmail.com>
2020-07-10 16:56:14 -0700

    Add site meta

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-  siteUrl: `https://eduardo-valencia.github.io/george-mutambuka`, // Site domain. Do not include a trailing slash!
+  siteUrl: `https://mystifying-newton-3bfa13.netlify.app`, // Site domain. Do not include a trailing slash!
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-  siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+  siteTitleMeta: `George Mutambuka`, // This allows an alternative site title for meta data for pages.
+  siteDescriptionMeta: `Professional 3D Graphics and Renders`, // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: `GM`, // Used for App manifest e.g. Mobile Home Screen
   siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
-  backgroundColor: `#e9e9e9`, // Used for Offline Manifest
+  backgroundColor: `black`, // Used for Offline Manifest
   themeColor: `#15171A`, // Used for Offline Manifest
 }
