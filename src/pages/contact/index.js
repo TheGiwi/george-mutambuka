deeda62fcdbd87a5fe44966b3b7d7cfcc6fd3621
George Mutambuka <georgegifted@gmail.com>
2020-09-09 13:15:24 +0300

    contact text (current issues)

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -16,7 +16,7 @@ export class Contact extends Component {
           <div className={this.extend('header')}>
             <h1>Contact me</h1>
             <p>
-              Contact me using the form below or email me at{' '}
+              CONTACT FORM IS CURRENTLY HAVING SENDING ISSUES SO PLEASE E-MAIL ME AT {' '}
               <a href={`mailto:${email}`}>{email} </a>
             </p>
           </div>
